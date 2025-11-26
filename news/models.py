766d367ede497e1b33d3dc0a603591a42af1233d
xurramovasliddin534 <xurramovasliddin534@gmail.com>
2025-11-26 16:57:26 +0500

    model yaratildi

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class NewsModel(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    rasm = models.ImageField(upload_to='new/images')
+
+    def __str__(self):
+        return self.title
